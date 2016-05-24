60266576df8ba951c25f40b7f03f5fd834ed71ab
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-24 15:16:37 -0500

    Bump to CLJ 1.8

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject me.arrdem/rate-gate "1.3.1"
   :description "Clojure mechanism for controlling the rate of actions"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
