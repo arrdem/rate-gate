6f2a6f73abd773b83310c09b1f44455289d76ab9
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-05-24 15:15:48 -0500

    Take ownership

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject rate-gate "1.3.1"
+(defproject me.arrdem/rate-gate "1.3.1"
   :description "Clojure mechanism for controlling the rate of actions"
   :dependencies [[org.clojure/clojure "1.4.0"]])
